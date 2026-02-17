aad7300ffc978b1a86456138b43573cdc9ff54b2
Anaf Ibn Shahibul <flourishing.a.c.c@gmail.com>
2026-02-17 17:33:10 +0600

    Add files via upload

---
+++ buildCache.js
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+
+const server = Bun.serve({
+  async fetch (req) {
+    const path = new URL(req.url).pathname.replace("/convert/", "") || "index.html";
+    const file = Bun.file(`${__dirname}/dist/${path}`);
+    if (!(await file.exists())) return new Response("Not Found", { status: 404 });
+    return new Response(file);
+  },
+  port: 8080
+});
+
+const browser = await puppeteer.launch({
+  headless: "new",
+  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+});
+
+const page = await browser.newPage();
+
+await Promise.all([
+  new Promise(resolve => {
+    page.on("console", msg => {
+      const text = msg.text();
+      if (text === "Built initial format list.") resolve();
+    });
+  }),
+  page.goto("http://localhost:8080/convert/index.html")
+]);
+
+const cacheJSON = await page.evaluate(() => {
+  return window.printSupportedFormatCache();
+});
+const outputPath = process.argv[2] || "cache.json";
+await Bun.write(outputPath, cacheJSON);
+
+await browser.close();
+server.stop();
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+      "@ffmpeg/ffmpeg",
+      "@sqlite.org/sqlite-wasm",
+    ]
+  },
+  base: "/convert/",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/@ffmpeg/core/dist/esm/ffmpeg-core.*",
+          dest: "wasm"
+        },
+        {
+          src: "node_modules/@imagemagick/magick-wasm/dist/magick.wasm",
+          dest: "wasm"
+        }
+      ]
+    }),
+    tsconfigPaths()
+  ]
+});
